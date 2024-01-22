aa74574a92791474a2168dc0405d95cbdccabc25
RickyMorel <ricardomorel0903@gmail.com>
2024-01-22 07:51:18 -0300

    InventoryItem

---
+++ src/Inventory/InventoryItemComponent.js
@@ -0,0 +1,44 @@
+import React from 'react';
+import { Color } from '../Colors';
+
+class InventoryItemComponent extends React.Component {
+
+    formatPrice = (numberString) => {
+        const number = parseFloat(numberString);
+      
+        if (isNaN(number)) {
+          return 'Invalid number';
+        }
+      
+        const formattedNumber = new Intl.NumberFormat('es-PY', {
+          style: 'currency',
+          currency: 'PYG',
+          minimumFractionDigits: 3,
+          maximumFractionDigits: 3,
+        }).format(number);
+      
+        return `${formattedNumber}`;
+    };
+
+    render() {
+        const { item, isInDailyInventory, handleClickCallback } = this.props;
+
+        return (
+        <div className="row list-item z-depth-2 border">
+            <div className="col s7">
+                <span className="client-name">{item.name}</span>
+            </div>
+            <div className="col s3">
+                <span className="client-name">{this.formatPrice(item.price)}</span>
+            </div>
+            <div className="col s2">
+                <a className={`waves-effect waves-light btn btn-small right ${isInDailyInventory == true ? Color.Second : Color.Fifth}`}>
+                <i className="material-icons" onClick={() => handleClickCallback(item, isInDailyInventory)}>{isInDailyInventory == true ? "arrow_back" : "arrow_forward"}</i>
+                </a>
+            </div>
+        </div>
+        );
+    }
+}
+
+export default InventoryItemComponent;
--- src/Inventory/InventoryScreen.js
+++ src/Inventory/InventoryScreen.js
@@ -1,8 +1,8 @@
 
-import React, { Component } from 'react';
-import ProductComponent from '../Products/ProductComponent';
-import SearchBar from '../Searchbar/Searchbar';
 import axios from 'axios';
+import React, { Component } from 'react';
+import SearchBar from '../Searchbar/Searchbar';
+import InventoryItemComponent from './InventoryItemComponent';
 
 class InventoryScreen extends Component {
     constructor(props) {
@@ -33,7 +33,6 @@ class InventoryScreen extends Component {
     };
 
     fetchGlobalConfig = async () => {
-        console.log("fetchGlobalConfig")
         try {
             const response = await axios.get(`${process.env.REACT_APP_HOST_URL}/inventory/getDayInventories`);
             this.setState({
@@ -43,13 +42,24 @@ class InventoryScreen extends Component {
         } catch (error) {}
     }
 
-    handleClick = (selectedDayNumber) => {
+    handleItemClick = (movedItem, isInDailyInventory) => {
+        var currentSelectedItems = [...this.state.selectedDayInventory.items]
+        currentSelectedItems.push(movedItem)
+        const newSelectedInventory = {day: this.state.selectedDayInventory.day, items: currentSelectedItems}
+
+        this.setState({
+            selectedDayInventory: newSelectedInventory
+        })
+
+        console.log("currentSelectedItems", currentSelectedItems)
+    }
+
+    handleDayTabClick = (selectedDayNumber) => {
         const allInventories = this.state.dayInventories
         this.setState({
             selectedDayInventory: allInventories[selectedDayNumber]
         })
-    }
-    
+    }   
 
     handleSearch = (filteredList) => {
         this.setState({
@@ -58,29 +68,31 @@ class InventoryScreen extends Component {
     }
     
     render() {
-        const allProductsList = this.state.filteredProducts?.map(x => (
-            <ProductComponent key={x.id} {...x} />
+        const {selectedDayInventory, filteredProducts} = this.state
+
+        const allProductsList = filteredProducts?.map(x => (
+            <InventoryItemComponent key={x.id} item={x} isInDailyInventory={false} handleClickCallback={this.handleItemClick} />
           ));
-        const selectedDayProductsList = this.state.selectedDayInventory?.items?.map(x => (
-            <ProductComponent key={x.id} {...x} />
+        const selectedDayProductsList = selectedDayInventory?.items?.map(x => (
+            <InventoryItemComponent key={x.id} item={x} isInDailyInventory={true} handleClickCallback={this.handleItemClick} />
         ));
 
         const navbarStyle = {
-        display: 'flex',
-        justifyContent: 'center',
+            display: 'flex',
+            justifyContent: 'center',
         };
 
         return (
         <div>
-            <nav className="transparent" style={navbarStyle}>
+            <nav className="transparent z-depth-0" style={navbarStyle}>
                 <ul className="center-align">
-                    <li onClick={() => this.handleClick(0)}><a>Lunes</a></li>
-                    <li onClick={() => this.handleClick(1)}><a>Martes</a></li>
-                    <li onClick={() => this.handleClick(2)}><a>Miercoles</a></li>
-                    <li onClick={() => this.handleClick(3)}><a>Jueves</a></li>
-                    <li onClick={() => this.handleClick(4)}><a>Viernes</a></li>
-                    <li onClick={() => this.handleClick(5)}><a>Sabado</a></li>
-                    <li onClick={() => this.handleClick(6)}><a>Domingo</a></li>
+                    <li onClick={() => this.handleDayTabClick(0)} className={`z-depth-${selectedDayInventory?.day == 0 ? "1" : "0"}`}><a className={`grey-text text-darken-2`}>Lunes</a></li>
+                    <li onClick={() => this.handleDayTabClick(1)} className={`z-depth-${selectedDayInventory?.day == 1 ? "1" : "0"}`}><a className={`grey-text text-darken-2`}>Martes</a></li>
+                    <li onClick={() => this.handleDayTabClick(2)} className={`z-depth-${selectedDayInventory?.day == 2 ? "1" : "0"}`}><a className={`grey-text text-darken-2`}>Miercoles</a></li>
+                    <li onClick={() => this.handleDayTabClick(3)} className={`z-depth-${selectedDayInventory?.day == 3 ? "1" : "0"}`}><a className={`grey-text text-darken-2`}>Jueves</a></li>
+                    <li onClick={() => this.handleDayTabClick(4)} className={`z-depth-${selectedDayInventory?.day == 4 ? "1" : "0"}`}><a className={`grey-text text-darken-2`}>Viernes</a></li>
+                    <li onClick={() => this.handleDayTabClick(5)} className={`z-depth-${selectedDayInventory?.day == 5 ? "1" : "0"}`}><a className={`grey-text text-darken-2`}>Sabado</a></li>
+                    <li onClick={() => this.handleDayTabClick(6)} className={`z-depth-${selectedDayInventory?.day == 6 ? "1" : "0"}`}><a className={`grey-text text-darken-2`}>Domingo</a></li>
                 </ul>
             </nav>
             <div className='row'>
