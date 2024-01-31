5ad17d49e85065b1e5d93041eea1ff43b679ef7d
RickyMorel <ricardomorel0903@gmail.com>
2024-01-30 21:08:12 -0300

    commit

--- src/Navbar.js
+++ src/Navbar.js
@@ -6,6 +6,10 @@ function Navbar() {
     <nav>
       <div className={`nav-wrapper ${Color.Second}`} style={{ "padding-left": '40px' }}>
         <a href="#!" className="brand-logo">Chatbot</a>
+        <ul id="nav-mobile" class="right hide-on-med-and-down">
+          <li style={{ "padding-right": '20px' }}>Emporio Aleman</li>
+          <li style={{ "padding-right": '20px' }}><img width="62" height="62" src="images/companyLogo.jpg" alt="" class="circle responsive-img"/></li>
+        </ul>
       </div>
     </nav>
   )
--- src/SideNav.js
+++ src/SideNav.js
@@ -2,51 +2,53 @@ import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
 import { Sidenav, Nav } from 'rsuite';
 import { Color } from './Colors';
+import { useLocation } from 'react-router-dom'
 
-class SideNav extends Component  {
-  render() {
-    // const { pathname } = this.props.location;
-    // console.log("pathname", pathname)
-    return (
-      <Sidenav>
-        <Sidenav.Body>
-          <Nav>
-            <Nav.Item className={`${Color.SideNav}`} active={true}>
-              <Link to="/">
-                <i className="material-icons left">cloud</i>
-                Cargar Datos
-              </Link>
-            </Nav.Item>
-            <Nav.Item className={`${Color.SideNav}`}>
-              <Link to="/blockChats">
-                <i className="material-icons left">contacts</i>
-                Bloquear Chat
-              </Link>
-            </Nav.Item>
-            <Nav.Item className={`${Color.SideNav}`}>
-              <Link to="/inventory">
-                  <i className="material-icons left">local_mall</i>
-                  Inventario
-              </Link>
-            </Nav.Item>
-            <Nav.Item className={`${Color.SideNav}`}>
-              <Link to="/orders">
-                <i className="material-icons left">shopping_cart</i>
-                Ver Pedidos
-              </Link>
-            </Nav.Item>
-            <Nav.Item className={`${Color.SideNav}`}>
-              <Link to="/dayLocation">
-                <i className="material-icons left">access_time</i>
-                Ver Tiempos y Lugares
-              </Link>
-            </Nav.Item>      
-          </Nav>
-        </Sidenav.Body>
-      </Sidenav>
-    );
-  }
-  
+function SideNav()  {
+  return (
+    <Sidenav>
+      <Sidenav.Body>
+        <Nav>
+          <Nav.Item className={GetNavItemColor('/')} active={true}>
+            <Link to="/">
+              <i className="material-icons left">cloud</i>
+              Cargar Datos
+            </Link>
+          </Nav.Item>
+          <Nav.Item className={GetNavItemColor('/blockChats')}>
+            <Link to="/blockChats">
+              <i className="material-icons left">contacts</i>
+              Bloquear Chat
+            </Link>
+          </Nav.Item>
+          <Nav.Item className={GetNavItemColor('/inventory')}>
+            <Link to="/inventory">
+                <i className="material-icons left">local_mall</i>
+                Inventario
+            </Link>
+          </Nav.Item>
+          <Nav.Item className={GetNavItemColor('/orders')}>
+            <Link to="/orders">
+              <i className="material-icons left">shopping_cart</i>
+              Ver Pedidos
+            </Link>
+          </Nav.Item>
+          <Nav.Item className={GetNavItemColor('/dayLocation')}>
+            <Link to="/dayLocation">
+              <i className="material-icons left">access_time</i>
+              Ver Tiempos y Lugares
+            </Link>
+          </Nav.Item>      
+        </Nav>
+      </Sidenav.Body>
+    </Sidenav>
+  );
 };
+
+function GetNavItemColor(navPath) {
+  const currentPath = useLocation().pathname;
+
+  return navPath == currentPath ? Color.Fifth : Color.SideNav
+}
 
 export default SideNav;
