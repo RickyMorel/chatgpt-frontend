ae5bc15f8344feffac2487f7a1b3c9ba2cc497a5
RickyMorel <ricardo.morel@posibillian.tech>
2024-03-13 14:46:52 -0300

    Icon flicker

--- src/ProblematicChats/ProblematicChatComponent.js
+++ src/ProblematicChats/ProblematicChatComponent.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Color } from '../Colors';
+import './styles.css';
 
 class ProblematicChatComponent extends React.Component {
 
@@ -30,11 +31,12 @@ class ProblematicChatComponent extends React.Component {
                 <span className="client-name">{this.getTime(data.createdDate)}</span>
             </div>
             <div className="col s2">
-                <a href={"https://wa.me/" + data.phoneNumber} target="_blank" rel="noopener noreferrer">{data.phoneNumber}</a>
+                <a href={"https://wa.me/" + data.phoneNumber} target="_blank" rel="noopener noreferrer" className="underlined-link">{data.phoneNumber}</a>
             </div>
             <div className="col s1">
-            {/* ${true == true ? Color.First : Color.Fifths} */}
-                <i style={{color: alertIconColor}} className={`material-icons`}>brightness_1</i>
+                <i style={{ color: alertIconColor }} className={`material-icons flicker`}>
+                    brightness_1
+                </i>
             </div>
         </div>
         );
