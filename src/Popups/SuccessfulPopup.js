94521164d223d6411dee73a3ce66b6ade039cb9c
Ricky_Mexx <ricardomorel0903@gmail.com>
2024-10-15 10:47:30 -0300

    popup

--- src/Popups/SuccessfulPopup.js
+++ src/Popups/SuccessfulPopup.js
@@ -5,25 +5,22 @@ import { faRectangleXmark, faSquareCheck } from '@fortawesome/free-regular-svg-i
 import CustomButton from '../Searchbar/CustomButton';
 import Utils from '../Utils';
 
-const SuccessfulPopup = ({ title }) => {
+const SuccessfulPopup = ({ title, closeFunc }) => {
+  const successMessage = "Guardo los datos correctamente."
+
   return (
-    <div>
-      <p style={{...CssProperties.LargeHeaderTextStyle, color: ColorHex.TextBody, textAlign: 'center'}}>Guardo Exitosamente</p>
+    <div class="row">
+      <div className="col-1">
+        <img width="60" height="60" src={"images/tick.png"} alt="" class="circle responsive-img"/>
+      </div>
+      <div className="col-9">
+        <p style={{...CssProperties.SmallHeaderTextStyle, color: ColorHex.TextBody, textAlign: 'center', paddingTop: '15px'}}>Guardo Exitosamente</p>
+      </div>
+      <div className="col-1">
+        <CustomButton onClickCallback={closeFunc} text="Cerrar"/>
+      </div>
     </div>
   );
-};
-
-const ulStyle = {
-  listStyleType: 'circle', 
-  // marginLeft: '20px', 
-  color: '#333',     
-};
-
-const liStyle = {
-  lineHeight: '1',
-  padding: '4px 0',  
-  ...CssProperties.MediumHeadetTextStyle, 
-  color: ColorHex.TextBody,
 };
 
 export default SuccessfulPopup;
